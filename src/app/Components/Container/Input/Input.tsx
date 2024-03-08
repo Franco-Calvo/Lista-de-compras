56926e3109148e15aceaa54066ba2cfb16d13fdc
Franco Calvo <fcalvo33@gmail.com>
2024-03-08 13:18:18 -0300

    Autocompletado en input

--- src/app/Components/Container/Input/Input.tsx
+++ src/app/Components/Container/Input/Input.tsx
@@ -1,9 +1,41 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "./Input.module.css";
 
-type InputProps = { value: string; label: string; width?: string };
+type InputProps = {
+  value: string;
+  label: string;
+  width?: string;
+  categories: string[];
+  onChange: (value: string) => void;
+};
 
-const Input = ({ value, label, width = "100%" }: InputProps) => {
+const Input = ({
+  value,
+  label,
+  width = "100%",
+  categories,
+  onChange,
+}: InputProps) => {
+  const [filteredCategories, setFilteredCategories] = useState<string[]>([]);
+  const [showSuggestions, setShowSuggestions] = useState<boolean>(false);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    onChange(inputValue);
+    if (!inputValue) {
+      setFilteredCategories([]);
+      setShowSuggestions(false);
+    } else {
+      setFilteredCategories(
+        categories.filter((category) =>
+          category.toLowerCase().includes(inputValue.toLowerCase())
+        )
+      );
+      setShowSuggestions(true);
+    }
+  };
+
   return (
     <div className={styles.inputContainer} style={{ width }}>
       <label>{label}</label>
@@ -12,7 +44,19 @@ const Input = ({ value, label, width = "100%" }: InputProps) => {
         type="text"
         placeholder="Busca un producto"
         value={value}
+        onChange={handleInputChange}
+        onBlur={() => setTimeout(() => setShowSuggestions(false), 100)}
+        onFocus={() => value && setFilteredCategories(categories)}
       />
+      {showSuggestions && (
+        <ul className={styles.suggestions}>
+          {filteredCategories.map((category, index) => (
+            <li key={index} onMouseDown={() => onChange(category)}>
+              {category}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
--- src/app/Components/Presenter/Home/Dashboard.tsx
+++ src/app/Components/Presenter/Home/Dashboard.tsx
@@ -1,23 +1,128 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "./Dashboard.module.css";
 import Input from "@/app/Components/Container/Input/Input";
 import InputWithSelect from "../../Container/InputWithSelect/InputWithSelect";
 import ProductCard from "../../Container/ProductCard/ProductCard";
 
+const categories = [
+  "Vinos",
+  "Aperitivos/Amargos",
+  "Licores",
+  "Espumantes",
+  "Fernet",
+  "Jugos en polvo",
+  "Jugos concentrados",
+  "Jugos listos para beber",
+  "Isotónicas",
+  "Energizantes",
+  "Gaseosas",
+  "Agua Mineral",
+  "Agua saborizada",
+  "Soda",
+  "Cervezas",
+  "Aderezos",
+  "Aceitunas",
+  "Atún",
+  "Sardinas",
+  "Arvejas",
+  "Choclo",
+  "Lentejas",
+  "Maíz",
+  "Salsas",
+  "Conservas de tomate",
+  "Arroz",
+  "Pastas Secas",
+  "Snacks",
+  "Caldos",
+  "Sopas",
+  "Puré instantáneo",
+  "Sal",
+  "Especias",
+  "Aceite",
+  "Vinagre",
+  "Aceto",
+  "Harina",
+  "Premezclas",
+  "Maizena",
+  "Polenta",
+  "Sémola",
+  "Azúcar",
+  "Galletitas",
+  "Vainillas",
+  "Pionono",
+  "Bizcochuelo",
+  "Esencia de vainilla",
+  "Coco rallado",
+  "Chocolate cobertura",
+  "Cereales",
+  "Mermeladas",
+  "Dulce de leche",
+  "Flan en polvo",
+  "Postre en polvo",
+  "Gelatina en polvo",
+  "Frutas enlatadas",
+  "Pan lactal",
+  "Cacao",
+  "Café",
+  "Té",
+  "Yerba",
+  "Mate cocido",
+  "Hamburguesas",
+  "Prefritos de pollo",
+  "Prefritos de pescado",
+  "Milanesas de soja",
+  "Pizza",
+  "Vegetales congelados",
+  "Papas fritas",
+  "Helado",
+  "Huevos",
+  "Crema de leche",
+  "Queso rallado",
+  "Fiambres",
+  "Quesos en trozos",
+  "Salchichas",
+  "Quesos untables",
+  "Levadura",
+  "Pastas frescas",
+  "Yogur",
+  "Postres",
+  "Gelatinas",
+  "Manteca",
+  "Margarina",
+  "Tapas para empanadas",
+  "Tapas pascualina",
+  "Leche",
+];
+
 const Dashboard = () => {
+  const [product, setProduct] = useState("Bebidas");
+
   return (
     <div className={styles.container}>
       <div className={styles.header} />
       <div className={styles.containerInputs}>
         <h1>Lista de compras</h1>
-        <Input label="Producto" value="Coca Cola 3LTS" width="100%" />
+        <Input
+          label="Producto"
+          value={product}
+          width="100%"
+          categories={categories}
+          onChange={(value) => setProduct(value)}
+        />
         <div className={styles.bottomInputs}>
           <InputWithSelect value="1" />
-          <Input label="Precio" value="$4200" width="60%" />
+          <Input
+            label="Precio"
+            value="$4200"
+            width="60%"
+            categories={[]}
+            onChange={() => {}}
+          />
           <button className={styles.addToCart}>+</button>
         </div>
       </div>
-      <ProductCard title="Bebidas" units="2 unidades" />
+      <ProductCard title={product} units="2 unidades" />
     </div>
   );
 };
